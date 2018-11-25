d38f9a61f59d512c66f303a5ffb505447adeb33a
Dave van der Goot <vanda403@school.lu>
2018-11-25 15:18:36 +0100

    Initial Commit

---
+++ PlaneTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaneTracker
+//
+//  Created by van der Goot Dave on 25/11/2018.
+//  Copyright © 2018 van der Goot Dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
